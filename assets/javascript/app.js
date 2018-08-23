805d022b6d667f3d2d5168b807a663f94666d44e
Sonam Mankame <sonamrohit.ms25@gmail.com>
2018-08-22 21:28:15 -0400

    modified background image added style added new div to display rates for 6 country

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,14 +15,14 @@ $.ajax({
     for (var i = 0; i < response.length; i++) {
       //  console.log(response[i].name);
       $("#inputCountry2").append('<option value ="' + response[i].name +
-      "-" + response[i].currencies[0].code + '">' + response[i].name +
+      "-" + response[i].currencies[0].code +"-"+ response[i].currencies[0].symbol +'">' + response[i].name +
       "-" + response[i].currencies[0].code + '</option>' + "<br>");
      
 
     }
     var country = $("#inputCountry2").val().split("-")
     var countryname;
-    console.log(countryname)
+    //console.log(countryname)
     // console.log(response[42].currencies[0].code);
     //console.log(response.name[0]);
     var queryURL = "http://www.apilayer.net/api/live?access_key=7b22f43a5fc06496f1f1de807d226428&currencies=CAD,CNY,EUR,GBP,MXN,SAR,INR";
@@ -35,26 +35,28 @@ $.ajax({
       // After data comes back from the request
       .then(function (response) {
         console.log(response.quotes);
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+ "CAD "+"<br>" +response.quotes.USDCAD + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"CYN "+"<br>" +response.quotes.USDCNY + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"EUR "+"<br>" +response.quotes.USDEUR + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"GBP "+"<br>" +response.quotes.USDGBP + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"INR "+"<br>" +response.quotes.USDINR + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"MXN "+"<br>" +response.quotes.USDMXN + "</div>");
-        $(".live-row-md-12").append("<div class = 'col-md-1'>"+"SAR "+"<br>" +response.quotes.USDSAR + "</div>");
+        $(".row").append("<div class = 'col-1'>"+ "USD"+"<br>" + "1" + "</div>");
+        $(".row").append("<div class = 'col-1'>"+ "CAD "+"<br>" +response.quotes.USDCAD + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"CYN "+"<br>" +response.quotes.USDCNY + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"EUR "+"<br>" +response.quotes.USDEUR + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"GBP "+"<br>" +response.quotes.USDGBP + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"INR "+"<br>" +response.quotes.USDINR + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"MXN "+"<br>" +response.quotes.USDMXN + "</div>");
+        $(".row").append("<div class = 'col-1'>"+"SAR "+"<br>" +response.quotes.USDSAR + "</div>");
       });
 //USDCAD: 1.30362USDCNY: 6.847503USDEUR: 0.864102USDGBP: 0.7749USDINR: 69.815033USDMXN: 18.876201USDSAR: 3.75075__proto__: Object
     //ul2
     $("#submit").on("click", function (event) {
       event.preventDefault()
+      $('#form').hide();
       $("#articles").empty()
       $('#display').empty()
       $('#rate').empty()
       country = $("#inputCountry2").val().split("-")
       var currencycode = country[1]
-      var currencycode = $("#inputCountry2").val();
+      //var currencycode = $("#inputCountry2").val();
             countryname = country[0]
-      //var field = currencycode.split('-');
+      var symbol = country[2];
       console.log("code:", currencycode);
       //console.log(field[1]);
 
@@ -92,16 +94,15 @@ $.ajax({
            //console.log(threeFromEnd);
            //console.log(country);
            $("#result").show();
-           $('#display').append('<p class="amount">'+ amount +' USD-$ = '+ reduceDecimaltoTwo+' '+ field[0]+'-'+ field[1] + '</p>')
+           $('#display').append('<p class="amount">'+ amount +' USD-$ = '+ reduceDecimaltoTwo +'-'+ currencycode + '-'+ symbol +'</p>')
 
-           $('#rate').append('Rate 1 USD = ' + value + " " + field[0])
+           $('#rate').append('Rate 1 USD = ' + value + " " + currencycode)
            var result = response
            console.log(result);
           //  +"<br>"
           //  +"<p>"+ amount+"</p>"+"<p>"+ currencycode+"</p>" +"<p>"+ calc +"</p>");
             // result.html("<h2"> + "This the country" + currencycode + "</h2>")
-           $('#form').hide();
-            $("#display").append(result)
+          $("#display").append(result)
         });
              getnews()
     var map = $("<img>").attr("src", "https://maps.googleapis.com/maps/api/staticmap?center=" + countryname +  "&zoom=8&size=600x300&maptype=roadmap&key=AIzaSyCY4ikZrWDzixZRNFVjpj7nwYtXar2ehKg")
