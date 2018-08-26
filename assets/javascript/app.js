a08ddd8f786f9141f5743dbfe7d729f7033166ca
Sonam Mankame <sonamrohit.ms25@gmail.com>
2018-08-26 14:40:30 -0400

    added new api for conversion added style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,9 @@
 
-
 let button = $("#back").hide()
 $(".card").hide();
 $(".card-header").hide();
 //check response from the 2 api selected
-var queryURL = "https://restcountries.eu/rest/v2/all?fields=name;currencies;flag";
+var queryURL = "https://restcountries.eu/rest/v2/all?fields=name;currencies";
 
 // Performing an AJAX request with the queryURL
 $.ajax({
@@ -15,110 +14,84 @@ $.ajax({
   .then(function (response) {
     console.log(response);
    
-   
-    //$("#inputCountry1").append()
-  //$("#img-1").append("<img src ="+ response[239].flag +">")
-$("#inputCountry1").append('<option value =' + response[239].name + '>'+ response[239].name + "-" + response[239].currencies[0].code + '</option>' + "<br>");
-// $("#inputCountry1").append()   
+    for (var i = 0; i < response.length; i++) {
+      //  console.log(response[i].name);
+        // $("#img-2").append("<img src ="+ response[i].flag +">")
+      $("#inputCountry1").append('<option value ="' + response[i].currencies[0].code  +'">'+ response[i].name +
+      "-" + response[i].currencies[0].code + '</option>' + "<br>");
+         }  
 
 
     for (var i = 0; i < response.length; i++) {
       //  console.log(response[i].name);
         // $("#img-2").append("<img src ="+ response[i].flag +">")
-      $("#inputCountry2").append('<option value ="' + response[i].name +
-      "-" + response[i].currencies[0].code +"-"+ response[i].currencies[0].symbol +'">'+ response[i].name +
-      "-" + response[i].currencies[0].code + '</option>' + "<br>");
-         }
+        $("#inputCountry2").append('<option value ="' + response[i].name
+        +'-'+ response[i].currencies[0].code  +'">'+ response[i].name +
+        "-" + response[i].currencies[0].code + '</option>' + "<br>");
+           }
 
-
-        
-    //$(".#img2").append("<div class = 'col-2'><img src ="+response[239].flag+">"+ "USD"+"<br>" + "1"+ "</div>");
-    var country = $("#inputCountry2").val().split("-")
-    var countryname;
-    //console.log(countryname)
-    // console.log(response[42].currencies[0].code);
-    //console.log(response.name[0]);
-    var queryURL = "http://www.apilayer.net/api/live?access_key=7b22f43a5fc06496f1f1de807d226428&currencies=CAD,CNY,EUR,GBP,MXN,SAR,INR";
-
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After data comes back from the request
-      .then(function (response) {
-        console.log(response);
-        $("#img2").append("<div class = 'col-auto '>"+"USD"+"<br>" + "1"+ "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+ "CAD"+"<br>" +response.quotes.USDCAD + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"CYN "+"<br>" +response.quotes.USDCNY + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"EUR "+"<br>" +response.quotes.USDEUR + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"GBP "+"<br>" +response.quotes.USDGBP + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"INR "+"<br>" +response.quotes.USDINR + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"MXN "+"<br>" +response.quotes.USDMXN + "</div>");
-        $("#img2").append("<div class = 'col-auto'>"+"SAR "+"<br>" +response.quotes.USDSAR + "</div>");
-      });
-
+          
+    
+  
     $("#submit").on("click", function (event) {
       event.preventDefault()
       $('#form').hide();
       $("#articles").empty()
       $('#display').empty()
-      $('#rate').empty()
+      $("#rate").empty()
       $("#map").empty()
+      $("#amount").empty()
+     // console.log(country1)
+      //console.log(country2)
       
-      country = $("#inputCountry2").val().split("-")
-      var currencycode = country[1]
-      //var currencycode = $("#inputCountry2").val();
-            countryname = country[0]
-      var symbol = country[2];
-      console.log("code:", currencycode);
-      //console.log(field[1]);
-
-      var queryURL = `http://www.apilayer.net/api/live?access_key=7b22f43a5fc06496f1f1de807d226428&currencies=${currencycode}`;
-
-      // Performing an AJAX request with the queryURL
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        // After data comes back from the request
-        .then(function (response) {
-          console.log(response.quotes)
-
-
-          var value
-          // rates loop of objecty
-          for (props in response.quotes){
-            console.log(response.quotes[props]);
-            value = response.quotes[props]
-          }
-
-          // var value = response.quotes["USD" + currencycode]
-          
+    var country1 = $("#inputCountry1").val()
+    //var countrycode1 = country1[1];
+    console.log(country1)
+  
+    var country = $("#inputCountry2").val().split("-")
+    var countryname = country[0];
+    var country2 = country[1];
+   
          var amount = $("#amount").val();
-           var calc = amount * value
-           console.log("calculated amount", calc);
-           console.log(response)
+         var query = country1 + '_' + country2;
+         console.log(query);
+  var url = 'https://free.currencyconverterapi.com/api/v6/convert?q='+ query + '&compact=ultra&';
+       
+                   $.ajax({
+                     url:url,
+                     method:'GET'
+                   }).done(function(result){
+                     console.log(result[query])  
+                 
+                   
+                   var val = result[query];
+                   if (val) {
+                     var total = val * amount;
+                     console.log( "total: "+ total);
+                     var reduceDecimaltoTwo = total.toFixed(2)
+                    
+           
+                      console.log(reduceDecimaltoTwo);
+                      
+                      $("#result").show();
+                      $('#display').append('<p class="amount">'+ amount + country1 +'-'+ reduceDecimaltoTwo + country2 + '</p>')
+           
+                      $('#rate').append('Rate 1'+" " + country1 + '=' + val + " " + country2)
+                    //  var result = response
+                      //console.log(result);
+                    
+                    // $("#display").append(result)
+     
+                   }
+                    else {
+                     var err = new Error("Value not found for " + query);
+                     console.log(err);
+                     
+                   }
+                  });
+                  getnews()
+                  console.log(countryname)
 
-           var reduceDecimaltoTwo = calc.toFixed(2)
-          // var threeFromEnd = reduceDecimaltoTwo.length -3
-          // var country = reduceDecimaltoTwo.slice(threeFromEnd, 0)
-
-           console.log(reduceDecimaltoTwo);
-           //console.log(threeFromEnd);
-           //console.log(country);
-           $("#result").show();
-           $('#display').append('<p class="amount">'+ amount +' USD-$ = '+ reduceDecimaltoTwo +'-'+ currencycode + '-'+ symbol +'</p>')
-
-           $('#rate').append('Rate 1 USD = ' + value + " " + currencycode)
-           var result = response
-           console.log(result);
-          //  +"<br>"
-          //  +"<p>"+ amount+"</p>"+"<p>"+ currencycode+"</p>" +"<p>"+ calc +"</p>");
-            // result.html("<h2"> + "This the country" + currencycode + "</h2>")
-          $("#display").append(result)
-        });
-             getnews()
     // var map = $("<img>").attr("src","https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyCY4ikZrWDzixZRNFVjpj7nwYtXar2ehKg")
 
 
@@ -126,11 +99,10 @@ $("#inputCountry1").append('<option value =' + response[239].name + '>'+ respons
     var map = $("<img>").attr("src", "https://maps.googleapis.com/maps/api/staticmap?center=" + countryname +  "&zoom=5&size=400x250&maptype=roadmap&key=AIzaSyCY4ikZrWDzixZRNFVjpj7nwYtXar2ehKg")
 
     $("#map").append(map)
-    // console.log(map)
+     console.log(countryname)
        $("#back").show(); 
        show();       
-        });
-
+     
         function getnews ()
         {
             var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
@@ -167,8 +139,10 @@ $("#inputCountry1").append('<option value =' + response[239].name + '>'+ respons
                         // th#img2 err;
                     });
                   }  
-       ////next ajax call
-    //  var qURL = ;
+                });
+
+              });
+           
 
     $("#back").on("click",function(){
       $('#form').show();
@@ -180,7 +154,8 @@ $("#inputCountry1").append('<option value =' + response[239].name + '>'+ respons
       $("#back").hide()
       $(".card").hide();
       $(".card-header").hide();
-      
+      $("#amount").empty()
+
     });
 
 function show(){
@@ -197,4 +172,48 @@ function show(){
     //on submit click take country code from field 1 and assign it to response.source
     //search for country code in field 2 in response.code field and get the rate
     //append this data to new screen with amount country 1 and country 2 and converted rate
-  });
+  
+
+  function convertCurrency(amount, fromCurrency, toCurrency) {
+   
+    fromCurrency = encodeURIComponent(fromCurrency);
+    toCurrency = encodeURIComponent(toCurrency);
+    var query = fromCurrency + '_' + toCurrency;
+    console.log(query);
+    var url = 'https://free.currencyconverterapi.com/api/v6/convert?q='+ query + '&compact=ultra&';
+  
+              $.ajax({
+                url:url,
+                method:'GET'
+              }).done(function(result){
+                console.log(result[query])  
+            
+              
+              var val = result[query];
+              if (val) {
+                var total = val * amount;
+                console.log( "total: "+ total);
+                var reduceDecimaltoTwo = total.toFixed(2)
+               
+      
+                 console.log(reduceDecimaltoTwo);
+                 
+                 $("#result").show();
+                 $('#display').append('<p class="amount">'+ amount +country1 + reduceDecimaltoTwo +'-'+ country2 + '</p>')
+      
+                // $('#rate').append('Rate 1 USD = ' + value + " " + currencycode)
+                 var result = response
+                 console.log(result);
+               
+                $("#display").append(result)
+
+
+
+              }
+               else {
+                var err = new Error("Value not found for " + query);
+                console.log(err);
+                
+              }
+             });
+} 
